dce18aaa9c5ad58eb6637c7ee24194eab419592c
Nguyễn Phương Quang <npquang240792@gmail.com>
2024-03-05 15:37:53 +0700

    FlatList

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,7 +2,8 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import { Product } from '../types';
 import Colors from '@/constants/Colors';
 
-export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+export const defaultPizzaImage =
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 type ProductListItemProps = {
   product: Product;
@@ -11,7 +12,11 @@ type ProductListItemProps = {
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -25,6 +30,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
   },
   title: {
     fontSize: 18,
